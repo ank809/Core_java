9b97ac9e50dade7a5d4d8b810df9e64b2f716793
Ankita <ankita12202002@gmail.com>
2023-05-01 01:45:56 +0530

    changes made

--- N_queens.java
+++ N_queens.java
@@ -1,60 +1,59 @@
 public class N_queens {
     public static void main(String[] args) {
         int n=4;
-        boolean board[][]= new boolean[n][n];
-       System.out.println(queen(board, 0)); 
+        boolean [][]board= new boolean[n][n];
+        System.out.println(queens(board, 0));
     }
-    
-static int queen(boolean [][]board, int row){
-    if(row==board.length){
-        display(board);
-        return 1;
-    }
-  // for placing the row and checking  for every row and column
-    int count=0;
-    for(int col=0; col<board.length; col++){
-        if(isSafe(board, row, col)){
-            board[row][col]= true;
-               count+= queen(board, row+1);
-               board[row][col]= false;
+    static int queens(boolean [][]board, int row){
+        if(row==board.length){
+            display(board);
+            System.out.println();
+            return 1;
+        
+        int count=0;
+        for(int col=0; col<board.length; col++){
+            if(isSafe(board, row, col)){
+                board[row][col]=true;
+                count+=queens(board, row+1);
+                board[row][col]=false;
+            }
         }
+        return count;
     }
-    return count;
-}
-static void display(boolean [][] board){
-    for(boolean[] row:board){
+    static boolean isSafe(boolean [][]board, int row, int col){
+        // check for vertical 
+        for(int i=0; i<row; i++){
+            if(board[i][col]){
+                return false;
+            }
+        }
+        // for left diagonal
+        int maxLeft= Math.min(row, col);
+        for(int i=1; i<=maxLeft; i++){
+            if(board[row-i][col-i]){
+                return false;
+            }
+        }
+        // for diagonal  right
+        int maxRight= Math.min(row, board.length-1-col);
+        for(int i=1; i<=maxRight; i++){
+            if(board[row-i][col+i]){
+                return false;
+            }
+        }
+        return true;
+    }
+ static void display(boolean [][]board){
+    for(boolean [] row:board){
         for(boolean element:row){
             if(element){
-                System.out.print("Q");
+                System.out.print("Q ");
             }
             else{
-                System.out.print("X"); 
+                System.out.print("X ");
             }
         }
         System.out.println();
     }
-}
-static boolean isSafe(boolean [][]board, int row, int col){
-    // for vertical
-    for(int i=0; i<row; i++){
-        if(board[row][col]){
-            return false;
-        }
-    }
-    // for diagonal left
-    int minLeft= Math.min(row, col);
-    for(int i=1; i<minLeft; i++){
-        if(board[row-i][col-i]){
-            return false;
-        }
-    }
-    // diagonal right
-    int maxRight= Math.min(row, board.length-1-col);
-    for(int i=1; i<maxRight; i++){
-        if(board[row-i][col+i]){
-            return false;
-        }
-    }
-    return true;
-}
+ }
 }

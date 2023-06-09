bc319774836d444c2b4f509f81ded89f96f47b38
ank809 <ankita51523@gmail.com>
2023-06-09 22:25:15 +0530

    linked list file ha been modified

--- Linked_List/Main.java
+++ Linked_List/Main.java
@@ -10,6 +10,25 @@ public class Main {
         list.InsertFirst(89);
         list.InsertFirst(67);
         list.display();
+        System.out.println();
+        // list.InsertLast(82);
+        // list.display();
+        // System.out.println();
+        list.Insert(25, 4);
+        list.display();
+       // System.out.println();
+        // list.deleteFirst();
+        // list.display();
+        System.out.println();
+        System.out.println(list.deleteLast());
+        list.display();
+        System.out.println();
+        list.delete(3);
+        list.display();
+        System.out.println();
+        System.out.println(list.search(90));
+        // list.display();
+
 
     }
     

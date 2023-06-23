07aa6b76555740516bd04449ff7a2fc0c91a2250
ank809 <ankita51523@gmail.com>
2023-06-23 21:24:53 +0530

    new files has been added

--- Linked_List/linked_theory/Rec_Main.java
+++ Linked_List/linked_theory/Rec_Main.java
@@ -2,7 +2,7 @@ package linked_theory;
 
 public class Rec_Main {
     public static void main(String[] args) {
-        Insert_rec list=  new Insert_rec();
+        //Insert_rec list=  new Insert_rec();
         // list.ins_rec(3, 56 );
 
     }
